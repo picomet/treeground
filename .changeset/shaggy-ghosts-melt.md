b61744a88612a2ea643dd329e94cfa5ed4eda68c
Al Mahdi <68297852+almahdi404@users.noreply.github.com>
2024-09-07 03:49:28 +0600

    fix(cli): use import.meta.url io __dirname

---
+++ .changeset/shaggy-ghosts-melt.md
@@ -0,0 +1,5 @@
+---
+"treeground": patch
+---
+
+fix(cli): use import.meta.url io \_\_dirname
--- index.js
+++ index.js
@@ -4,4 +4,6 @@ import path from "node:path";
 process.env.PORT = 7533;
 process.env.HOST = "localhost";
 
-import(path.join(process.cwd(), ".output", "server", "index.mjs"));
+import(
+  path.join(path.dirname(import.meta.url), ".output", "server", "index.mjs")
+);
