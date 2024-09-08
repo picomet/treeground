4eed932a99fdf5c6142f7c176835f0716a65d786
Al Mahdi <68297852+almahdi404@users.noreply.github.com>
2024-09-08 18:43:59 +0600

    ui: use gray bg color

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -16,7 +16,7 @@ export default createHandler(() => (
           />
           {assets}
         </head>
-        <body>
+        <body class="bg-gray-100">
           <div id="app">{children}</div>
           {scripts}
         </body>
